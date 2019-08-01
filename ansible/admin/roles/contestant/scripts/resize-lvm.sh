05f8120b40f3cc7574655865425b2a6a21c72b38
Ismayil Hasanov <ismailhasanov@gmail.com>
2019-08-01 12:06:16 +0400

    add: LVM partition expand script

---
+++ ansible/admin/roles/contestant/scripts/resize-lvm.sh
@@ -0,0 +1,9 @@
+# Variables
+LV=/dev/ubuntu-vg/ubuntu-lv
+SIZE=+100G
+
+# Extend the given logical volume by the size
+lvextend -L $SIZE $LV
+
+# Resize the filesystem on the given logical volume
+resize2fs $LV
